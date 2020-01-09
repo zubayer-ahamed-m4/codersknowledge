db28340df9f6bb98ac8f03906c61e38ab9e28c5e
zubay <zubay@DESKTOP-4G23OV9>
2020-01-09 23:59:12 +0600

    1st commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Bootstrap Example</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+        <style type="text/css">
+            .header-wrapper{
+                background-color: #454E5C;
+                min-height: 200px;
+                padding: 0px 20px;
+            }
+            .content-wrapper{
+                background-color: #f0f0f0;
+                min-height: 200px;
+                padding: 0px 20px;
+            }
+            .footer-wrapper{
+                background-color: #33363B;
+                min-height: 100px;
+                padding: 0px 20px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="header-wrapper"></div>
+        <div class="content-wrapper"></div>
+        <div class="footer-wrapper"></div>
+    </body>
+</html>
